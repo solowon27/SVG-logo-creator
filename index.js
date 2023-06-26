eb69beb7661bf4eca6f9e08fdd1e17930d6bcdce
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-06-26 10:16:53 -0600

    upperCase and textColor methods added to user inputs

--- index.js
+++ index.js
@@ -16,32 +16,40 @@ const questions = [
       message: 'Enter desired color:'
     },
     {
-      type: 'input',
-      name: 'text',
-      message: 'Enter text for the shape:',
-      validate: function (input) {
-        if (input.trim().length === 3) {
-          return true;
-        } else {
-          return 'Please enter exactly three characters.';
-        }
+    type: 'input',
+    name: 'text',
+    message: 'Enter text for the shape:',
+    validate: function (input) {
+      if (input.trim().length === 3) {
+        return true;
+      } else {
+        return 'Please enter exactly three characters.';
       }
+    },
+    filter: function (input) {
+      return input.toUpperCase();
     }
-  ];
+  },
+  {
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter desired text color:'
+  }
+];
   
   inquirer.prompt(questions).then(answers => {
-    const { type, color, text } = answers;
+    const { type, color, text, textColor} = answers;
   
     let shape;
     switch (type) {
       case 'rectangle':
-        shape = new Rectangle(type, color, text);
+        shape = new Rectangle(type, color, text, textColor);
         break;
       case 'square':
-        shape = new Square(type, color, text);
+        shape = new Square(type, color, text, textColor);
         break;
       case 'circle':
-        shape = new Circle(type, color, text);
+        shape = new Circle(type, color, text, textColor);
         break;
       default:
         throw new Error(`Invalid shape type: ${type}`);
