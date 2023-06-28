dc770ad545055a9f9787b35af61a03e54020ce4b
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-06-27 20:27:00 -0600

    put comments for each function and class functionality

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const fs = require('fs'); 
-const inquirer = require('inquirer'); 
+//welcome to SVG logo creator application
+// i am going to show you the functionality of basic code blocks
+
+const fs = require('fs'); //this a file system which allow us to read and write file
+const inquirer = require('inquirer'); //inquirer module allow us to prompt the user for inputs
 
 const { Rectangle, Square, Circle } = require('./lib/shapes'); 
 
@@ -9,8 +12,8 @@ const messages = [
     'and you can test the application by running \x1b[32m npm test\x1b[0m or \x1b[32m npx jest\x1b[0m \n',
   ];
   
-  function instruction() {  
-    let x = 0;
+  function instruction() {  //this function is include a message that appeared in the begining when the index.js run 
+    let x = 0; //it is just an instruction for a user 
   
     const intervalId = setInterval(function () { 
       if (x < messages.length) {
@@ -18,11 +21,11 @@ const messages = [
         x++;
       } else {
         clearInterval(intervalId); 
-        init();
+        init(); //we have this funnction that will begin the main application and it will execute after the instruction() ended
       }
     }, 500);
   }
-const questions = [ 
+const questions = [ //this is our question array to prompt the user for input 
     {
       type: 'list',
       name: 'type',
@@ -38,14 +41,14 @@ const questions = [
     type: 'input',
     name: 'text',
     message: 'Enter \x1b[36m text\x1b[0m for logo Name:',
-    validate: function (input) { 
+    validate: function (input) {  //validate the input to be exactly three characters
       if (input.trim().length === 3) {
         return true;
-      } else {
+      } else { //if not it will return an error message and ask the user to input again
         return 'Please enter exactly three characters.';
       }
     },
-    filter: function (input) { 
+    filter: function (input) { //filter the text input to uppercase 
       return input.toUpperCase();
     }
   },
@@ -55,11 +58,11 @@ const questions = [
     message: 'Enter desired \x1b[36m text color:\x1b[0m:'
   }
 ];
-  function init() {
+  function init() { //this is our function which prompt the user for input and store the answers in the answer variable
   inquirer.prompt(questions).then(answers => { 
     const { type, color, text, textColor} = answers;
   
-    let shape;
+    let shape; //switch statment will do create the shape based the user input from the shape.js shape renders
     switch (type) { 
       case 'rectangle':
         shape = new Rectangle(type, color, text, textColor);
@@ -74,12 +77,11 @@ const questions = [
         throw new Error(`Invalid shape type: ${type}`);
     }
   
-    fs.writeFile('logo.svg', shape.render(), err => { 
+    fs.writeFile('logo.svg', shape.render(), err => {  //fs.writeFile allow us here to write a logo.svg in our directory
       if (err) throw err;
       console.log('logo.svg created!');
     });
   });
 }
 
-instruction(); 
-  
+instruction(); //our app begin here
