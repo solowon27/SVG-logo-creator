36c8ffc8d2ea383cf57b59b3e2e2a9394e5db9e0
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-06-28 18:58:04 -0600

    comments

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+//hello welcome to the SVG logo creator application 
+//i am going to show you the functionality of some code blocks and 
+//we will test our application using npx jest or npm test
+//finally we run application to create a logo using SVG format
+//lets go
 
-const fs = require('fs'); 
-const inquirer = require('inquirer');
 
-const { Rectangle, Square, Circle, Triangle} = require('./lib/shapes'); 
+const fs = require('fs'); //this is our file system module which allow us to read and write files
+const inquirer = require('inquirer'); //inquirer module to prompt the user for input
+
+const { Rectangle, Square, Circle, Triangle} = require('./lib/shapes'); //we requiring the child classes here from our shape.js to create the shape based on the input
 
 const messages = [ 
     '\nWelcome to SVG logo creator!',
@@ -19,11 +25,11 @@ const messages = [
         x++;
       } else {
         clearInterval(intervalId); 
-        init(); 
+        init(); //we have this function to start the main app after the messages are deliver
       }
     }, 500);
-  }
-const questions = [ //this is our question array to prompt the user for input 
+  }//this is our function that loads at first when our application start and it is just a message for a user as an instruction
+const questions = [ //this is our question array to prompt the user for input
     {
       type: 'list',
       name: 'type',
@@ -33,13 +39,13 @@ const questions = [ //this is our question array to prompt the user for input
     {
       type: 'input',
       name: 'color',
-      message: 'Enter desired \x1b[36m color:\x1b[0m ',
+      message: 'Enter desired \x1b[36m color:\x1b[0m even using hex color code ',
       validate: function (input) {
         const validColors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black', 'white', 'gray', 'brown', 'pink', 'purple',];
-        const validHex = /^#[0-9A-F]{6}$/i;
-        if (validColors.includes(input.toLowerCase()) || validHex.test(input)) { //validate the input to be a valid color
+        const validHex = /^#[0-9A-F]{6}$/i; //this is the reqular expression to validate the hex color
+        if (validColors.includes(input.toLowerCase()) || validHex.test(input)) {  //check if the input is either valid hex color or valid color name 
           return true;
-        } else { //if not it will return an error message and ask the user to input again
+        } else { 
           return 'Please enter a valid color.';
       }
     }
@@ -48,14 +54,14 @@ const questions = [ //this is our question array to prompt the user for input
     type: 'input',
     name: 'text',
     message: 'Enter \x1b[36m text\x1b[0m for logo Name:',
-    validate: function (input) {  //validate the input to be exactly three characters
+    validate: function (input) {  //this validate if the input is less than 3 character it return an error message and then return to the question again
       if (input.trim().length < 3) {
         return 'Please enter at least three characters.';
-      } else { //if not it will return an error message and ask the user to input again
+      } else { 
         return true;
       }
     },
-    filter: function (input) { //filter the text input to uppercase 
+    filter: function (input) { 
       return input.toUpperCase();
     }
   },
@@ -65,12 +71,12 @@ const questions = [ //this is our question array to prompt the user for input
     message: 'Enter desired \x1b[36m text color:\x1b[0m:'
   }
 ];
-  function init() { //this is our function which prompt the user for input and store the answers in the answer variable
+  function init() { //this is our initiate application by prompt the user for input and store the answer in the answer variable
   inquirer.prompt(questions).then(answers => { 
     const { type, color, text, textColor} = answers;
   
-    let shape; //switch statment will do create the shape based the user input from the shape.js shape renders
-    switch (type) { 
+    let shape; 
+    switch (type) { //this switch statment will do create the shape based on the user input
       case 'rectangle':
         shape = new Rectangle(type, color, text, textColor);
         break;
@@ -87,7 +93,7 @@ const questions = [ //this is our question array to prompt the user for input
         throw new Error(`Invalid shape type: ${type}`);
     }
   
-    fs.writeFile('./lib/logo.svg', shape.render(), err => {  //fs.writeFile allow us here to write a logo.svg in our directory
+    fs.writeFile('./lib/logo.svg', shape.render(), err => {  //fs.writeFile allow us here to write a logo.svg in lib directory
       if (err) throw err;
       console.log('logo.svg created!');
     });
